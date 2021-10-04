2bedc0334481ebad30de8093d8a320003b200bd8
grazielab <67067719+grazielab@users.noreply.github.com>
2021-10-04 15:37:16 -0300

    Add files via upload
    Mudança de repositório

---
+++ Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        Calculadora calcula = new Calculadora();
+
+        double taxaISS = calcula.cISS(200);
+        double taxaIOF = calcula.cIOF(600);
+        double taxaIR = calcula.cIR(800);
+
+        System.out.printf("O valor do imposto ISS foi de R$ %.4f \n", taxaISS);
+        System.out.printf("O valor do imposto IOF foi de R$ %.4f \n", taxaIOF);
+        System.out.printf("O valor do imposto IR foi de R$ %.4f \n", taxaIR);
+
+    }
+}
