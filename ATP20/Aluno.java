43ee8ab109c04e02b855b5df92c23b455bd68160
Graziela B B Beckhauser <grazi_1996@hotmail.com>
2021-10-05 15:53:09 -0300

    concluida as classes de pessoa, funcionario, coordenador e aluno

--- ATP20/Aluno.java
+++ ATP20/Aluno.java
@@ -1,3 +1,22 @@
-public class Aluno {
+public class Aluno extends Pessoa{
+
+    private String curso;
+    private char turma;
+
+    // CURSO
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+
+    // CURSO
+    public void setTurma(char turma) {
+        this.turma = turma;
+    }
+    public char getTurma() {
+        return turma;
+    }
     
 }
--- ATP20/Coordenador.java
+++ ATP20/Coordenador.java
@@ -1,3 +1,23 @@
-public class Coordenador {
+public class Coordenador extends Funcionario {
+
+    private String cursoC;
+    private double bonus = 0.10;
+
+     // CURSO COORDENADO
+     public void setCurso(String cursoC) {
+        this.cursoC = cursoC;
+    }
+    public String getCurso() {
+        return cursoC;
+    }
+
+     // BONUS
+     public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+    public double getBonus() {
+        return bonus;
+    }
+
     
 }
