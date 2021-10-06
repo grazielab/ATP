3921f1cb49067911254fd5d17760b5e910f5bd72
Graziela B B Beckhauser <grazi_1996@hotmail.com>
2021-10-06 13:55:22 -0300

    finalizada classe conta

---
+++ ATP21/Conta.java
@@ -0,0 +1,21 @@
+public class Conta{
+
+    private String saldo;
+    private int codigo;
+
+    // SALDO
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    public String getSaldo() {
+        return saldo;
+    }
+
+    // CODIGO
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public int getCodig() {
+        return codigo;
+    }
+}
